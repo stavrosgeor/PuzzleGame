d6b0b2fd0ee9bc12f408f598997b711e778bdb0a
georg <georg@STAVROS>
2024-02-07 20:12:09 +0200

    Add project files.

---
+++ Puzzle/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.levelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.beginnerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.advantageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Move #0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.colorToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(805, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.levelToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "&Game";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // levelToolStripMenuItem
+            // 
+            this.levelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.beginnerToolStripMenuItem,
+            this.advantageToolStripMenuItem});
+            this.levelToolStripMenuItem.Name = "levelToolStripMenuItem";
+            this.levelToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.levelToolStripMenuItem.Text = "Level";
+            // 
+            // beginnerToolStripMenuItem
+            // 
+            this.beginnerToolStripMenuItem.Name = "beginnerToolStripMenuItem";
+            this.beginnerToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.beginnerToolStripMenuItem.Text = "Beginner";
+            this.beginnerToolStripMenuItem.Click += new System.EventHandler(this.beginnerToolStripMenuItem_Click);
+            // 
+            // advantageToolStripMenuItem
+            // 
+            this.advantageToolStripMenuItem.Name = "advantageToolStripMenuItem";
+            this.advantageToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.advantageToolStripMenuItem.Text = "Advance";
+            this.advantageToolStripMenuItem.Click += new System.EventHandler(this.advantageToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.otherToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.colorToolStripMenuItem.Text = "&Color";
+            // 
+            // otherToolStripMenuItem
+            // 
+            this.otherToolStripMenuItem.Name = "otherToolStripMenuItem";
+            this.otherToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.otherToolStripMenuItem.Text = "&Other";
+            this.otherToolStripMenuItem.Click += new System.EventHandler(this.otherToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 481);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Puzzle Game";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem levelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem beginnerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem advantageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otherToolStripMenuItem;
+    }
+}
+
---
+++ Puzzle/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Stavros Georgiou Puzzle 
+
+namespace Puzzle
+{
+    public partial class Form1 : Form
+    {
+        private int clickCount = 0;
+        private Button[] btnNum;
+        private int emptyIndex = 8; // Index of the empty button
+        private int spacing = 10;   // Declare spacing at the class level
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeButtons();
+        }
+        // Initialize puzzle buttons
+        private void InitializeButtons()
+        {
+            btnNum = new Button[9];
+
+            int buttonSize = 80; // Increase button size for a bigger puzzle
+            int spacing = 10;    // Increase the spaces between buttons
+
+            for (int i = 0; i < 9; i++)
+            {
+                btnNum[i] = new Button();
+                btnNum[i].Size = new System.Drawing.Size(buttonSize, buttonSize);
+                btnNum[i].Click += BtnNum_Click;
+                btnNum[i].Tag = i + 1;
+
+                // Set the text on the buttons (except the last one)
+                if (i < 8)
+                {
+                    btnNum[i].Text = (i + 1).ToString();
+                    btnNum[i].ForeColor = System.Drawing.Color.Red; // Set text color to red
+                    btnNum[i].Font = new System.Drawing.Font(btnNum[i].Font.FontFamily, 16, System.Drawing.FontStyle.Bold); // Set font size to 16
+                }
+
+                int row = i / 3;
+                int col = i % 3;
+
+                // Center the puzzle on the form
+                int x = (this.ClientSize.Width - (3 * buttonSize + 2 * spacing)) / 2 + col * (buttonSize + spacing);
+                int y = (this.ClientSize.Height - (3 * buttonSize + 2 * spacing)) / 2 + row * (buttonSize + spacing);
+
+                btnNum[i].Location = new System.Drawing.Point(x, y);
+                this.Controls.Add(btnNum[i]);
+            }
+
+            NewGame();
+        }
+        // Handle button click event
+        private void BtnNum_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            int clickedIndex = Array.IndexOf(btnNum, clickedButton);
+
+            if (IsAdjacent(clickedIndex, emptyIndex))
+            {
+                SwapButtons(clickedIndex, emptyIndex);
+                emptyIndex = clickedIndex;
+
+                clickCount++;
+                label1.Text = $"Move # {clickCount}";
+
+                if (Solved())
+                {
+                    MessageBox.Show("Congratulations! You've solved the puzzle.");
+                    NewGame();
+                    clickCount = 0;
+                    label1.Text = "Move #0";
+                }
+
+                // Update button locations after each click
+                UpdateButtonLocations();
+            }
+        }
+        // Update button locations based on the puzzle state
+        private void UpdateButtonLocations()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                int row = i / 3;
+                int col = i % 3;
+                int x = (this.ClientSize.Width - (3 * btnNum[i].Width + 2 * spacing)) / 2 + col * (btnNum[i].Width + spacing);
+                int y = (this.ClientSize.Height - (3 * btnNum[i].Height + 2 * spacing)) / 2 + row * (btnNum[i].Height + spacing);
+                btnNum[i].Location = new System.Drawing.Point(x, y);
+            }
+        }
+        // Check if two buttons are adjacent
+        private bool IsAdjacent(int index1, int index2)
+        {
+            int row1 = index1 / 3;
+            int col1 = index1 % 3;
+            int row2 = index2 / 3;
+            int col2 = index2 % 3;
+
+            // Check if the two buttons are horizontally or vertically adjacent
+            return (Math.Abs(row1 - row2) == 1 && col1 == col2) || (Math.Abs(col1 - col2) == 1 && row1 == row2);
+        }
+        // Swap positions of two buttons
+        private void SwapButtons(int index1, int index2)
+        {
+            Button temp = btnNum[index1];
+            btnNum[index1] = btnNum[index2];
+            btnNum[index2] = temp;
+
+            UpdateButtonLocations();
+        }
+        // Check if the puzzle is in a solved state
+        private bool Solved()
+        {
+            // Check for the two provided goal states
+            bool isGoalState1 = btnNum[0].Text == "1" && btnNum[1].Text == "2" && btnNum[2].Text == "3" &&
+                                btnNum[3].Text == "4" && btnNum[4].Text == "5" && btnNum[5].Text == "6" &&
+                                btnNum[6].Text == "7" && btnNum[7].Text == "8" && btnNum[8].Text == "";
+
+            bool isGoalState2 = btnNum[0].Text == "1" && btnNum[1].Text == "2" && btnNum[2].Text == "3" &&
+                                btnNum[3].Text == "8" && btnNum[4].Text == "" && btnNum[5].Text == "4" &&
+                                btnNum[6].Text == "7" && btnNum[7].Text == "6" && btnNum[8].Text == "5";
+
+            return isGoalState1 || isGoalState2;
+        }
+        // Randomly allocate numbers function
+        private void NewGame()
+        {
+            List<int> numbers = Enumerable.Range(1, 8).ToList();
+            Random random = new Random();
+
+            // Shuffle the numbers using Fisher-Yates shuffle
+            for (int i = numbers.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(0, i + 1);
+                int temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+
+            // Assign the shuffled numbers to buttons
+            for (int i = 0; i < 8; i++)
+            {
+                btnNum[i].Text = numbers[i].ToString();
+                btnNum[i].Tag = numbers[i];
+                SetButtonStyle(btnNum[i]); // Set the font style for each numbered button
+            }
+
+            // Set the style for the empty button
+            btnNum[8].Text = "";
+            btnNum[8].Tag = 9;
+            SetButtonStyle(btnNum[8]);
+
+            // Ensure the puzzle is solvable
+            if (!IsSolvable())
+            {
+                // If not solvable, swap the positions of the last two buttons
+                SwapButtons(7, 8);
+            }
+
+            // Update the emptyIndex after shuffling
+            emptyIndex = Array.IndexOf(btnNum, btnNum.First(b => b.Text == ""));
+        }
+
+        private void SetButtonStyle(Button button)
+        {
+            button.ForeColor = System.Drawing.Color.Red; // Set text color to red
+            button.Font = new System.Drawing.Font(button.Font.FontFamily, 16, System.Drawing.FontStyle.Bold); // Set font size to 16
+        }
+        private bool IsSolvable()
+        {
+            // Count the number of inversions
+            int inversions = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = i + 1; j < 8; j++)
+                {
+                    // Skip empty tile represented by ""
+                    if (btnNum[i].Text != "" && btnNum[j].Text != "" && (int)btnNum[i].Tag > (int)btnNum[j].Tag)
+                    {
+                        inversions++;
+                    }
+                }
+            }
+            // If the grid width is even, the number of inversions should be odd for a solvable puzzle
+            if (inversions % 2 == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NewGame();
+            clickCount = 0;
+            label1.Text = "Move #0";
+        }
+        //Menu Strip function
+        private void InitializeMenu()
+        {
+            // Create a MenuStrip
+            MenuStrip menuStrip = new MenuStrip();
+
+            // Create menu items
+            ToolStripMenuItem beginnerMenuItem = new ToolStripMenuItem("Beginner");
+            beginnerMenuItem.Click += beginnerToolStripMenuItem_Click;
+
+            ToolStripMenuItem advancedMenuItem = new ToolStripMenuItem("Advanced");
+            advancedMenuItem.Click += advantageToolStripMenuItem_Click;
+
+            ToolStripMenuItem newGameMenuItem = new ToolStripMenuItem("New Game");
+            newGameMenuItem.Click += newGameToolStripMenuItem_Click;
+
+            // Add menu items to the MenuStrip
+            menuStrip.Items.Add(beginnerMenuItem);
+            menuStrip.Items.Add(advancedMenuItem);
+            menuStrip.Items.Add(newGameMenuItem); // Add the "New Game" menu item
+
+            ToolStripMenuItem colorMenuItem = new ToolStripMenuItem("Color");
+
+            // Add sub-menu items under Color
+            ToolStripMenuItem otherMenuItem = new ToolStripMenuItem("Other");
+            otherMenuItem.Click += otherToolStripMenuItem_Click;
+
+            // Add sub-menu items to Color menu
+            colorMenuItem.DropDownItems.Add(otherMenuItem);
+
+            // Add Color menu to the MenuStrip
+            menuStrip.Items.Add(colorMenuItem);
+
+            // Set the MenuStrip for the form
+            this.Controls.Add(menuStrip);
+            this.MainMenuStrip = menuStrip;
+        }
+
+        private enum Difficulty
+        {
+            Beginner,
+            Advanced
+        }
+
+        private void advantageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Implement logic for Advanced difficulty
+            SetDifficulty(Difficulty.Advanced);
+        }
+
+        private void beginnerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Implement logic for Beginner difficulty
+            SetDifficulty(Difficulty.Beginner);
+        }
+
+        // Method to adjust the puzzle difficulty
+        private void SetDifficulty(Difficulty difficulty)
+        {
+            switch (difficulty)
+            {
+                case Difficulty.Beginner:
+                    NewGame(); // Reset the puzzle for the Beginner difficulty
+                    break;
+
+                case Difficulty.Advanced:
+                    // Implement logic for Advanced difficulty
+                    // For example, shuffle the puzzle more aggressively
+                    for (int i = 0; i < 100; i++)
+                    {
+                        int randomIndex1 = random.Next(0, 8);
+                        int randomIndex2 = random.Next(0, 8);
+                        SwapButtons(randomIndex1, randomIndex2);
+                    }
+                    break;
+
+                // Add more difficulty levels if needed
+
+                default:
+                    break;
+            }
+
+            // Update the emptyIndex after adjusting the puzzle
+            emptyIndex = Array.IndexOf(btnNum, btnNum.First(b => b.Text == ""));
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Stavros Georgiou");
+        }
+
+        private void otherToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Show the ColorDialog control when "Other" menu item is clicked
+            ColorDialog colorDialog = new ColorDialog();
+
+            // Optionally set initial color
+            colorDialog.Color = this.BackColor; // or any default color
+
+            // Show the dialog and get the result
+            DialogResult result = colorDialog.ShowDialog();
+
+            // If the user clicks OK, set the background color to the selected color
+            if (result == DialogResult.OK)
+            {
+                this.BackColor = colorDialog.Color;
+            }
+        }
+    }
+}
